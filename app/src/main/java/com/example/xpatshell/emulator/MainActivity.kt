845c1549682eb922a51de18c25c44027bf9481ec
mavstar12 <142558978+mavstar12@users.noreply.github.com>
2025-12-12 16:09:41 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/xpatshell/emulator/MainActivity.kt
+++ app/src/main/java/com/example/xpatshell/emulator/MainActivity.kt
@@ -57,6 +57,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+window.decorView.systemUiVisibility = (
+    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+    or View.SYSTEM_UI_FLAG_FULLSCREEN
+    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+)
+
         // Simple layout programmatic (safe if layout resource isn't present)
         webView = WebView(this)
         setContentView(webView)
