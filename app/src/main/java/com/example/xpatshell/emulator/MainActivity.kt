47263d4e29b41c642b45955a4980e06f5c3cf3b9
mavstar12 <142558978+mavstar12@users.noreply.github.com>
2025-12-17 03:37:25 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/xpatshell/emulator/MainActivity.kt
+++ app/src/main/java/com/example/xpatshell/emulator/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.core.view.WindowInsetsCompat
 
