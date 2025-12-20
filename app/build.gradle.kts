189af142a328252358116e0138d7dc86d39dc048
mavstar12 <142558978+mavstar12@users.noreply.github.com>
2025-12-20 21:09:09 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,5 +56,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview:1.6.0")
     debugImplementation("androidx.compose.ui:ui-tooling:1.6.0")
     implementation("androidx.core:core-ktx:1.13.1")
-implementation("androidx.compose.foundation:foundation:1.6.0")
+    implementation("androidx.compose.foundation:foundation:1.6.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
 }
